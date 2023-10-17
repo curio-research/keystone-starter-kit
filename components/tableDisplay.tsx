6eb80e946c0a9a5ba3e13e868ace9531b3041ad4
kdonthi <kaushikdr@gmail.com>
2023-10-17 14:36:22 -0700

    Add Table component to display table data

--- components/tableDisplay.tsx
+++ components/tableDisplay.tsx
@@ -1,26 +1,64 @@
-import React, {Component} from "react";
-import PropTypes from "prop-types";
-import {TableAccessor} from "../store/accessors";
+import React from "react";
+import {TableAccessor} from "../store/tableAccessor";
 import {useSelector} from "react-redux";
-import {store, StoreType, TableType} from "../store/store";
+import {TableType, WorldType} from "../store/store";
+import {WithID} from "../store/schemas";
 
 
-function TableDisplay<T extends {Id: number}> (props: {accessor: TableAccessor<T>}) {
-    const slice = useSelector((state: TableType<T>) => state.get(props.accessor.name()))!
-    const divs = new Array<T>()
+interface TableProps<T extends WithID> {
+    table: TableType<T>
+    accessor: TableAccessor<T>
+}
 
-    slice.forEach(function (value, key, map) {
-        const val = props.accessor.get(value.Id)!
-        divs.push(val)
-    })
+export function TableDisplay<T extends {Id: number}> (props: {accessor: TableAccessor<T>}) {
+    const accessor = props.accessor;
+    const slice = useSelector((state: WorldType) => state.get(accessor.name()))!
 
     return (
         <React.Fragment>
-            {
-                divs.map(function (value, index, array) {
-                    return <div>{value as any}</div>
-                })
-            }
+            <div>Table Name: {accessor.name()}</div>
+            <Table table={slice} accessor={accessor} />
         </React.Fragment>
     )
+}
+
+function Table<T extends WithID>(props: TableProps<T>) {
+    const {table, accessor} = props;
+
+    const anyVal = accessor.getAny(table)
+    if (anyVal === undefined) {
+        return null
+    }
+
+    const columnNames = new Array<string>();
+    for (const field in anyVal) {
+        columnNames.push(field);
+    }
+
+    const allEntities = accessor.allEntities(table)
+    return <React.Fragment>
+        <table>
+            {
+                <tr>
+                    {
+                        columnNames.map((value, index) => {
+                            return <th key={index}>{value}</th>
+                        })
+                    }
+                </tr>
+            }
+            {
+                allEntities.map((entity, index) => {
+                    const obj = accessor.get(table, entity)! as any
+                    return <tr key={index}>
+                        {
+                            columnNames.map((columnName, index) => {
+                                return <td key={index}>{obj[columnName]}</td>
+                            })
+                        }
+                    </tr>
+                })
+            }
+        </table>
+    </React.Fragment>
 }
