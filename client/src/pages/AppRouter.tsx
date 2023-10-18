977af6d3740c5ac2c550c5d40ffd83c7c9b65a98
Kevin Z <zkw917@gmail.com>
2023-10-18 16:38:43 -0700

    Add 404 not found route

--- client/src/pages/AppRouter.tsx
+++ client/src/pages/AppRouter.tsx
@@ -1,12 +1,22 @@
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import TableExplorer from "../components/TableExplorer";
+import { Box, Text } from "@chakra-ui/react";
 
 export const AppRouter = () => {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path={"/explore"} element={<TableExplorer />} />
+        <Route path="/explore" element={<TableExplorer />} />
+        <Route path="*" element={<NotFoundPage />} />
       </Routes>
     </BrowserRouter>
+  );
+};
+
+const NotFoundPage = () => {
+  return (
+    <Box p="10">
+      <Text fontSize="2xl">Page not found :-/</Text>
+    </Box>
   );
 };
